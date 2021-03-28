6507fef17410ad60b63b9042f113a99a2f39a3aa
Basti Hoffmann <bo.hoffi@gmail.com>
2021-03-28 11:44:17 +0200

    chore: remove old code

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -4,12 +4,6 @@ export const checkProjectType = (project: workspaces.ProjectDefinition, checkTyp
     return project.extensions.projectType === checkType;
 };
 
-// export const checkOutputPath = (project: workspaces.ProjectDefinition): boolean => {
-//     return !!project.extensions.architect
-//         && !!(project.extensions.architect as JsonObject).build
-//         && !!((project.extensions.architect as JsonObject).build as JsonObject).options
-//         && !!(((project.extensions.architect as JsonObject).build as JsonObject).options as JsonObject).outputPath;
-// };
 export const checkTargetOption = (project: workspaces.ProjectDefinition, target: string, option: string): boolean => {
     if (project.targets.get(target)?.options === null) {
         return false;
@@ -18,12 +12,6 @@ export const checkTargetOption = (project: workspaces.ProjectDefinition, target:
     return options.option !== null;
 };
 
-// export const addArchitect = (project: workspaces.ProjectDefinition, architect: string, builder: string, options: any): void => {
-//     (project.extensions.architect as JsonObject)[architect] = {
-//         builder,
-//         options
-//     };
-// };
 export const addProjectTarget = (project: workspaces.ProjectDefinition, target: string, builder: string, options: any): void => {
     project.targets.add({
         name: target,
