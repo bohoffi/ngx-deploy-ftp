582dccc677c7b10ddb8f584ef56129a8de99a2c7
bohoffi <bo.hoffi@gmail.com>
2022-12-17 23:40:20 +0100

    refactor!: configure first application project
    BREAKING CHANGE: when the `project` parameter on `ng-add` is omitted the first available application project is choosen due to removed `defaultProject` option of the workspace defintion

--- src/ng-add/ng-add.ts
+++ src/ng-add/ng-add.ts
@@ -1,49 +1,61 @@
 import {
-    SchematicContext,
-    SchematicsException,
-    Tree
-} from '@angular-devkit/schematics';
-import { workspaces } from '@angular-devkit/core';
-import { NgAddOptions } from '../schema/ng-add-schema';
-import { createWorkspaceHost } from '../utils/workspace';
-import { addProjectTarget, checkTargetOption, checkProjectType } from '../utils/project';
+  SchematicContext,
+  SchematicsException,
+  Tree,
+} from "@angular-devkit/schematics";
+import { workspaces } from "@angular-devkit/core";
+import { NgAddOptions } from "../schema/ng-add-schema";
+import { createWorkspaceHost } from "../utils/workspace";
+import {
+  addProjectTarget,
+  checkTargetOption,
+  checkProjectType,
+} from "../utils/project";
 
-export const ngAdd = (options: NgAddOptions) =>
-    async (tree: Tree, context: SchematicContext) => {
+export const ngAdd = (options: NgAddOptions) => async (tree: Tree) => {
+  const workspaceHost = createWorkspaceHost(tree);
+  const { workspace } = await workspaces.readWorkspace("/", workspaceHost);
 
-        const workspaceHost = createWorkspaceHost(tree);
-        const { workspace } = await workspaces.readWorkspace('/', workspaceHost);
+  if (!options.project) {
+    const firstApplicationProjectIndex = Array.from(
+      workspace.projects.values()
+    ).findIndex(
+      (projectDefinition) =>
+        (projectDefinition.extensions.projectType as string) === "application"
+    );
+    if (firstApplicationProjectIndex >= 0) {
+      options.project = Array.from(workspace.projects.keys()).at(
+        firstApplicationProjectIndex
+      );
+    }
+  }
 
-        if (!options.project) {
-            if (workspace.extensions.defaultProject) {
-                options.project = workspace.extensions.defaultProject as string;
-            } else {
-                throw new SchematicsException(
-                    'No Angular project selected and no default project in the workspace'
-                );
-            }
-        }
+  if (!options.project) {
+    throw new SchematicsException(
+      "No project selected and no application project in workspace"
+    );
+  }
 
-        const project = workspace.projects.get(options.project);
-        if (!project) {
-            throw new SchematicsException(`Invalid project name: ${options.project}`);
-        }
+  const project = workspace.projects.get(options.project);
+  if (!project) {
+    throw new SchematicsException(`Invalid project name: ${options.project}`);
+  }
 
-        if (!checkProjectType(project, 'application')) {
-            throw new SchematicsException(
-                `Deploy requires an Angular project type of "application" in angular.json`
-            );
-        }
+  if (!checkProjectType(project, "application")) {
+    throw new SchematicsException(
+      `Deploy requires an Angular project type of "application" in angular.json`
+    );
+  }
 
-        if (!checkTargetOption(project, 'build', 'outputPath')) {
-            throw new SchematicsException(
-                `Cannot read the output path (architect.build.options.outputPath) of the Angular project "${options.project}" in angular.json`
-            );
-        }
+  if (!checkTargetOption(project, "build", "outputPath")) {
+    throw new SchematicsException(
+      `Cannot read the output path (architect.build.options.outputPath) of the Angular project "${options.project}" in angular.json`
+    );
+  }
 
-        addProjectTarget(project, 'deploy', 'ngx-deploy-ftp:deploy', {});
+  addProjectTarget(project, "deploy", "ngx-deploy-ftp:deploy", {});
 
-        // Apply changes
-        await workspaces.writeWorkspace(workspace, workspaceHost);
-        return tree;
-    };
+  // Apply changes
+  await workspaces.writeWorkspace(workspace, workspaceHost);
+  return tree;
+};
