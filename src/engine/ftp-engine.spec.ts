01fc1541614bea1910a28d576004a08ef21da610
bohoffi <bo.hoffi@gmail.com>
2022-12-17 23:35:20 +0100

    test: update tests

--- src/engine/ftp-engine.spec.ts
+++ src/engine/ftp-engine.spec.ts
@@ -1,48 +1,52 @@
-import { DeployOptions } from '../schema/deploy-schema';
+import { DeployOptions } from "../schema/deploy-schema";
 
-import * as engine from './ftp-engine';
+import * as engine from "./ftp-engine";
 
 const minRequiredOptions: DeployOptions = {
-    host: 'i-am-the-host',
-    username: 'user',
-    password: 'passw0rd'
+  host: "i-am-the-host",
+  username: "user",
+  password: "passw0rd",
 };
 
 const missingHostOptions: DeployOptions = {
-    username: 'user',
-    password: 'passw0rd'
+  username: "user",
+  password: "passw0rd",
 };
 
 const missingUsernameOptions: DeployOptions = {
-    host: 'i-am-the-host',
-    password: 'passw0rd'
+  host: "i-am-the-host",
+  password: "passw0rd",
 };
 
 const missingPasswordOptions: DeployOptions = {
-    host: 'i-am-the-host',
-    username: 'user'
+  host: "i-am-the-host",
+  username: "user",
 };
 
-describe('ngx-deploy-ftp ftp-engine', () => {
+describe("ngx-deploy-ftp ftp-engine", () => {
+  it("should pass option verification", () => {
+    const spy = jest.spyOn(engine, "verifyRequiredOptions");
 
-    it('should pass option verification', () => {
+    engine.verifyRequiredOptions(minRequiredOptions);
 
-        const spy = spyOn(engine, 'verifyRequiredOptions').and.callThrough();
+    expect(spy).toHaveBeenCalled();
+  });
 
-        engine.verifyRequiredOptions(minRequiredOptions);
+  it("should fail on missing host options", () => {
+    expect(() => engine.verifyRequiredOptions(missingHostOptions)).toThrowError(
+      "Cannot deploy the application without a host"
+    );
+  });
 
-        expect(spy).toHaveBeenCalled();
-    });
+  it("should fail on missing username options", () => {
+    expect(() =>
+      engine.verifyRequiredOptions(missingUsernameOptions)
+    ).toThrowError("Cannot deploy the application without a username");
+  });
 
-    it('should fail on missing host options', () => {
-        expect(() => engine.verifyRequiredOptions(missingHostOptions)).toThrowError('Cannot deploy the application without a host');
-    });
-
-    it('should fail on missing username options', () => {
-        expect(() => engine.verifyRequiredOptions(missingUsernameOptions)).toThrowError('Cannot deploy the application without a username');
-    });
-
-    it('should fail on missing password options', () => {
-        expect(() => engine.verifyRequiredOptions(missingPasswordOptions)).toThrowError('Cannot deploy the application without a password');
-    });
+  it("should fail on missing password options", () => {
+    expect(() =>
+      engine.verifyRequiredOptions(missingPasswordOptions)
+    ).toThrowError("Cannot deploy the application without a password");
+  });
 });
--- src/ng-add/ng-add.spec.ts
+++ src/ng-add/ng-add.spec.ts
@@ -1,65 +1,95 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
-import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
-import { Schema as ApplicationOptions } from '@schematics/angular/application/schema';
+import { Tree } from "@angular-devkit/schematics";
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from "@angular-devkit/schematics/testing";
+import { Schema as WorkspaceOptions } from "@schematics/angular/workspace/schema";
+import { Schema as ApplicationOptions } from "@schematics/angular/application/schema";
 
-import { NgAddOptions } from '../schema/ng-add-schema';
+import { NgAddOptions } from "../schema/ng-add-schema";
 
-const collectionPath = require.resolve('../collection.json');
+const collectionPath = require.resolve("../collection.json");
 
 const workspaceOptions: WorkspaceOptions = {
-  name: 'workspace',
-  newProjectRoot: 'tests',
-  version: '9.1.4',
+  name: "workspace",
+  newProjectRoot: "tests",
+  version: "15.0.4",
 };
 
-const appOptions: ApplicationOptions = { name: 'test-app' };
+const appOptions: ApplicationOptions = { name: "test-app" };
 
 const addOptions: NgAddOptions = {
-  project: appOptions.name
+  project: appOptions.name,
 };
 
 const emptyAddOptions: NgAddOptions = {};
 
 const corruptAddOptions: NgAddOptions = {
-  project: 'i-am-not-existing'
+  project: "i-am-not-existing",
 };
 
-describe('ng add ngx-deploy-ftp', () => {
-  const testRunner = new SchematicTestRunner('schematics', collectionPath);
+describe("ng add ngx-deploy-ftp", () => {
+  const testRunner = new SchematicTestRunner("schematics", collectionPath);
 
   async function initAngularProject(): Promise<UnitTestTree> {
     const appTree = await testRunner
-      .runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions)
+      .runExternalSchematicAsync(
+        "@schematics/angular",
+        "workspace",
+        workspaceOptions
+      )
       .toPromise();
     return await testRunner
-      .runExternalSchematicAsync('@schematics/angular', 'application', appOptions, appTree)
+      .runExternalSchematicAsync(
+        "@schematics/angular",
+        "application",
+        appOptions,
+        appTree
+      )
       .toPromise();
   }
 
-  it('fails with a missing tree', async () => {
-    await expect(testRunner.runSchematicAsync('ng-add', {}, Tree.empty()).toPromise()).rejects.toThrow();
+  it("fails with a missing tree", async () => {
+    await expect(
+      testRunner.runSchematicAsync("ng-add", {}, Tree.empty()).toPromise()
+    ).rejects.toThrow();
   });
 
-  it('fails with a non-existing project', async () => {
-    await expect(testRunner.runSchematicAsync('ng-add', corruptAddOptions, Tree.empty()).toPromise()).rejects.toThrow();
+  it("fails with a non-existing project", async () => {
+    await expect(
+      testRunner
+        .runSchematicAsync("ng-add", corruptAddOptions, Tree.empty())
+        .toPromise()
+    ).rejects.toThrow();
   });
 
-  it('adds ngx-deploy-ftp to the default project', async () => {
+  it("adds ngx-deploy-ftp to the default project", async () => {
     let appTree = await initAngularProject();
-    appTree = await testRunner.runSchematicAsync('ng-add', emptyAddOptions, appTree).toPromise();
-    const angularJson = JSON.parse(appTree.readContent('/angular.json'));
+    appTree = await testRunner
+      .runSchematicAsync("ng-add", emptyAddOptions, appTree)
+      .toPromise();
+    const angularJson = JSON.parse(appTree.readContent("/angular.json"));
 
-    expect(angularJson.projects[appOptions.name].architect.deploy).toBeDefined();
-    expect(angularJson.projects[appOptions.name].architect.deploy.builder).toBe('ngx-deploy-ftp:deploy');
+    expect(
+      angularJson.projects[appOptions.name].architect.deploy
+    ).toBeDefined();
+    expect(angularJson.projects[appOptions.name].architect.deploy.builder).toBe(
+      "ngx-deploy-ftp:deploy"
+    );
   });
 
-  it('adds ngx-deploy-ftp to an existing project', async () => {
+  it("adds ngx-deploy-ftp to an existing project", async () => {
     let appTree = await initAngularProject();
-    appTree = await testRunner.runSchematicAsync('ng-add', addOptions, appTree).toPromise();
-    const angularJson = JSON.parse(appTree.readContent('/angular.json'));
+    appTree = await testRunner
+      .runSchematicAsync("ng-add", addOptions, appTree)
+      .toPromise();
+    const angularJson = JSON.parse(appTree.readContent("/angular.json"));
 
-    expect(angularJson.projects[appOptions.name].architect.deploy).toBeDefined();
-    expect(angularJson.projects[appOptions.name].architect.deploy.builder).toBe('ngx-deploy-ftp:deploy');
+    expect(
+      angularJson.projects[appOptions.name].architect.deploy
+    ).toBeDefined();
+    expect(angularJson.projects[appOptions.name].architect.deploy.builder).toBe(
+      "ngx-deploy-ftp:deploy"
+    );
   });
 });
